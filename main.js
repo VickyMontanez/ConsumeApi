925b1c3cf6597c31308ce2ca19a9be7d963091e3
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-23 22:01:52 -0500

    feat: :sparkles: Add new function filter
    Add a new function of a filter to search any character

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import getApiData from "../storage/wsMyContent.js"
+
+document.addEventListener("keyup", e =>{
+    if(e.target.matches("#search")){
+        getApiData.articles.document.querySelectorAll(".articulo").forEach(personaje =>{
+            personaje.textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+                ?personaje.classList.remove(".filtro")
+                :personaje.classList.add(".filtro")
+        })
+    }
+})
