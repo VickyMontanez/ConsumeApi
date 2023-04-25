f3d990b52a267ccd308502a7c7beb5e395c063cd
VickyMontanezCampus <vmontanez707@gmail.com>
2023-04-25 13:28:55 -0500

    feat: :fire: Code debugging
    debugging bad code done with bad practices

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-import getApiData from "./storage/wsMyContent.js";
-/* import { article } from "./storage/wsMyContent.js"; */
 
-document.addEventListener("keyup", e =>{
-    if(e.target.matches("#search")){
-        getApiData.document.querySelectorAll(".articulo").forEach(personaje =>{
-            personaje.textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
-                ?personaje.classList.remove(".filtro")
-                :personaje.classList.add(".filtro")
-        })
-    }
-})
