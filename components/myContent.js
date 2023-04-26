0295cac10c276f1fda384023081deb883d60ca0d
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-26 03:13:04 -0500

    feat: :sparkles: Add new Methods
    Add new functions for buttons and pages

--- components/myContent.js
+++ components/myContent.js
@@ -1,7 +1,7 @@
 export default{
     showApi(){
         const WS = new Worker("./storage/wsMyContent.js", {type: "module"});
-        WS.postMessage({module:"wsShowApi", data:""}) 
+        WS.postMessage({module:"wsShowApi", data:""});
         WS.addEventListener("message", (e)=>{
             const container = document.querySelector("#main");
             container.innerHTML="";
@@ -21,5 +21,28 @@ export default{
             const searchTerm = searchInput.value;
             wsSr.postMessage({module:"wsSearch", data: searchTerm});
         });
-    }
+    },
+
+    botones(){
+        const wsbtn = new Worker ("./storage/wsMyContent.js", {type: "module"});
+        wsbtn.addEventListener("message", (e)=>{
+            const containerbtn = document.querySelector("#containerbtn");
+            containerbtn.innerHTML="";
+            containerbtn.insertAdjacentHTML("beforeend", e.data);
+        });
+        wsbtn.postMessage({module:"createBotones"});
+    },
+
+    pagesTemplate(){
+        const wspage = new Worker("./storage/wsMyContent.js", {type:"module"});
+        wspage.addEventListener("message", (e)=>{
+            const container = document.querySelector("#main");
+            container.innerHTML="";
+            container.insertAdjacentHTML("beforeend", e.data);
+        });
+        const urlParams = new URLSearchParams(window.location.search);
+        const page = urlParams.get('page') || 1;
+        wspage.postMessage({module:"createTemplate", num: page});
+    },
+
 };
