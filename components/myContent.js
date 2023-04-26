46ae312fa0ea71c5f738c71187e1a12b31d7b3d5
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-25 22:07:48 -0500

    feat: :sparkles: Add a Search by name thod bound a worker
    Add a new Search by name method bound to the worker and bound to the main.js

--- components/myContent.js
+++ components/myContent.js
@@ -6,7 +6,20 @@ export default{
             const container = document.querySelector("#main");
             container.innerHTML="";
             container.insertAdjacentHTML("beforeend",e.data);
-        })
-
+        });
+    },
+    
+    search(){
+        const wsSr = new Worker("./storage/wsMyContent.js",{type: "module"});
+        wsSr.addEventListener("message", (e)=>{
+            const container = document.querySelector("#main");
+            container.innerHTML="";
+            container.insertAdjacentHTML("beforeend", e.data);
+        });
+        const searchInput = document.querySelector("#searchField");
+        searchInput.addEventListener("keyup", ()=>{
+            const searchTerm = searchInput.value;
+            wsSr.postMessage({module:"wsSearch", data: searchTerm});
+        });
     }
-}
+};
