49235349922713c23131d5ed3758d56dea1814b4
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-25 22:04:38 -0500

    feat: :sparkles: Add another method in the Worker
    Add the Search by name method in a functional Worker

--- storage/wsMyContent.js
+++ storage/wsMyContent.js
@@ -16,6 +16,24 @@ let wsMyContent = {
         </article>`
         });
         return templateList;
+    },
+    
+    async wsSearch(searchTerm){
+        let dataSearch = await api.getByName(searchTerm);
+        let templateListSearch = '';
+        dataSearch.results.forEach(personaje =>{
+            templateListSearch += `<article>
+            <div class="img">       
+                <img src="${personaje.image}" alt="" srcset="">
+            </div>
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+            <p>${personaje.gender}</p>
+            <p>${personaje.species}</p>
+            <p>${personaje.location.name}</p>
+        </article>` 
+        });
+        return templateListSearch;
     }
 };
 
