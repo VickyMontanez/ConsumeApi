6c2db292ecc4dbdc301cdea6a43db90e76a6d44a
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-25 21:02:30 -0500

    feat: :sparkles: Introduce new methods Api
    Introduce new methods for get data of the Api and export for use them soon

---
+++ API/api.js
@@ -0,0 +1,50 @@
+/* Se debe obtener toda la data de los personajes de la API */
+const getApi = async()=>{
+    try{
+        const data = await fetch(`https://rickandmortyapi.com/api/character`);
+        const result = await data.json();
+        return result;
+    }catch (error) {
+        console.log(error);;
+    };
+};
+
+/* Se obtiene la data de los personajes por su Id */
+const getById = async(id)=>{
+    try{
+        const data = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+        const results = await data.json();
+        return results;
+    } catch(error){
+        console.log(error);
+    };
+};
+
+/* Se obtiene la data de los personajes por su Name */
+const getByName = async(name)=>{
+    try{
+        const data = await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+        const results = await data.json();
+        return results
+    }catch(error){
+        console.log(error);
+    };
+};
+
+/* Se obtiene la data de los personajes por su page*/
+const getByPage = async(page)=>{
+    try{
+        const data = await fetch(`"https://rickandmortyapi.com/api/character/?page=${page}`);
+        const results = await data.json();
+        return results
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export default{
+    getApi,
+    getById,
+    getByName,
+    getByPage
+}
