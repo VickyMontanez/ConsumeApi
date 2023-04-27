8f509c2300dceb79d6498f4bb8d7075962ec475a
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-26 23:17:47 -0500

    feat: :sparkles: Pagination code and new clear and clean code deploy
    New function in the worker for pagination Api and new function for every worker better implementation for the page

--- storage/wsMyContent.js
+++ storage/wsMyContent.js
@@ -1,40 +1,44 @@
 import api from "../API/api.js";
+/*  Función para NO sobreescribir */
 
-let wsMyContent = {
-    async wsShowApi(){
-        const data = await api.getApi();
-        let templateList = '';
-        data.results.forEach(personaje =>{
-            templateList += `<article>
+function crearCharactersList(lista) {
+    let templateList = '';
+    lista.forEach(personaje =>{
+        templateList +=  `<article>
+        <div class="content">
             <div class="img">       
                 <img src="${personaje.image}" alt="" srcset="">
             </div>
-            <h2 class="my-3">${personaje.name}</h2>
-            <p>${personaje.status}</p>
-            <p>${personaje.gender}</p>
-            <p>${personaje.species}</p>
-            <p>${personaje.location.name}</p>
-        </article>`
-        });
-        return templateList;
+            <div class="info my-3">
+            <h2 >${personaje.name}</h2>
+            <ul class="mt-4">
+                <li><p>${personaje.status}</p></li>
+                <li><p>${personaje.gender}</p></li>
+                <li><p>${personaje.species}</p></li>
+                <li><p>${personaje.location.name}<p></p></li>
+            </ul> 
+    </div>
+        </div>
+    </article>` 
+    });
+    return templateList;
+}
+
+/* funciones asíncronas con data obtenida desde Api */
+let wsMyContent = {
+    async wsShowApi(){
+        const data = await api.getApi();
+        return crearCharactersList(data.results);
     },
     
     async wsSearch(searchTerm){
         let dataSearch = await api.getByName(searchTerm);
-        let templateListSearch = '';
-        dataSearch.results.forEach(personaje =>{
-            templateListSearch += `<article>
-            <div class="img">       
-                <img src="${personaje.image}" alt="" srcset="">
-            </div>
-            <h2 class="my-3">${personaje.name}</h2>
-            <p>${personaje.status}</p>
-            <p>${personaje.gender}</p>
-            <p>${personaje.species}</p>
-            <p>${personaje.location.name}</p>
-        </article>` 
-        });
-        return templateListSearch;
+        return crearCharactersList(dataSearch.results);
+    },
+
+    async wsPage(pageNumber){
+        let dataPage = await api.getByPage(pageNumber);
+        return crearCharactersList(dataPage.results);
     },
 
 };
